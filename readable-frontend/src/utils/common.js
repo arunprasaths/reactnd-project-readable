55bee410a0ff77676f8c43d8259d2dd61ece58a0
Arun Prasath <arunprasath316@gmail.com>
2018-04-01 15:19:07 +0530

    added the other constants

--- readable-frontend/src/utils/common.js
+++ readable-frontend/src/utils/common.js
@@ -1,3 +1,7 @@
 export const  formatDate = (timestamp) => {
     return  new Date(timestamp).toDateString()
 }
+
+export const ROOT_URL ='http://localhost:3001'
+export const AUTH_HEADERS = {headers: { 'Authorization': 'whatever-you-want'}};
+
